fe3bd0ca20393ac890da5ab4fa5c349af59cd7e3
yhwang15 <donkassu@gmail.com>
2022-11-15 15:16:31 +0900

    [FEAT] : Comment Entity 구현

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/comment/entity/Comment.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/comment/entity/Comment.java
@@ -1,20 +1,48 @@
 package com.codestates.mainproject.oneyearfourcut.domain.comment.entity;
 
 
+import com.codestates.mainproject.oneyearfourcut.domain.artwork.entity.ArtWork;
+import com.codestates.mainproject.oneyearfourcut.domain.gallery.entity.Gallery;
+import com.codestates.mainproject.oneyearfourcut.domain.member.entity.Member;
 import com.codestates.mainproject.oneyearfourcut.global.auditable.Auditable;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+
+@NoArgsConstructor
+@Getter
 @Entity
 public class Comment extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
-    private Long galleryId;
-    private Long memberId;
-    private String content;
-    private Long artworkId;
+
+    private String content; // 댓글 내용
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member memberId; // 작성자 회원 id
+
+    @ManyToOne
+    @JoinColumn(name = "galley_id")
+    private Gallery galleryId;  //작품이 아닌 전시관 전체 댓글일때
+
+    @ManyToOne
+    @JoinColumn(name = "artwork_id")
+    private ArtWork artWorkId;  //작품에 달린 댓글일때.
+
+    @Builder
+    public Comment(Long commentId, String content, Member memberId, Gallery galleryId, ArtWork artWorkId){
+        this.commentId = commentId;
+        this.content = content;
+        this.memberId = memberId;
+        this.galleryId = galleryId;
+        this.artWorkId = artWorkId;
+    }
+
+
 }
