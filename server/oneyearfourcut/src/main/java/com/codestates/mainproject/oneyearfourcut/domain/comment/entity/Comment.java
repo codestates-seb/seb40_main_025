2173a4ae8e2968c088eebe1fd1bcd3dcfe01e0aa
yhwang15 <donkassu@gmail.com>
2022-11-15 22:38:14 +0900

    [FEAT] : Comment Entity 완료

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/comment/entity/Comment.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/comment/entity/Comment.java
@@ -7,44 +7,42 @@ import com.codestates.mainproject.oneyearfourcut.domain.artwork.entity.ArtWork;
 import com.codestates.mainproject.oneyearfourcut.domain.gallery.entity.Gallery;
 import com.codestates.mainproject.oneyearfourcut.domain.member.entity.Member;
 import com.codestates.mainproject.oneyearfourcut.global.auditable.Auditable;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
-@NoArgsConstructor
+
+
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Comment extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
 
+    @OneToMany(mappedBy = "member")
+    private List<Comment> commentList = new ArrayList<>();  //댓글 리스트, targetEntity
+
     private String content; // 댓글 내용
 
     @ManyToOne
     @JoinColumn(name = "member_id")
-    private Member memberId; // 작성자 회원 id
-
-    @ManyToOne
-    @JoinColumn(name = "artwork_id")
-    private ArtWork artWorkId;  //작품에 달린 댓글일때.
+    private Member member; // 작성자 회원 id
 
     @ManyToOne
     @JoinColumn(name = "galley_id")
-    private Gallery galleryId;  //작품이 아닌 전시관 전체 댓글일때
+    private Gallery gallery;  //작품이 아닌 전시관 전체 댓글일때
 
-    @Builder
-    public Comment(Long commentId, String content, Member memberId, Gallery galleryId, ArtWork artWorkId){
-        this.commentId = commentId;
-        this.content = content;
-        this.memberId = memberId;
-        this.galleryId = galleryId;
-        this.artWorkId = artWorkId;
-    }
+    private Long artWorkId;  //작품에 달린 댓글일때.
+
+    @OneToMany(mappedBy = "comment")
+    private List<Reply> replyList = new ArrayList<>(); // 대댓글, targetEntity
 
 
 }
