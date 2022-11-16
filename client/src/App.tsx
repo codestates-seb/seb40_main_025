a35333cf037b094b7576ebe0f48325c2ba9133b8
KDY <kkdy21@naver.com>
2022-11-16 21:30:15 +0900

    ✨FEAT : Theme provider 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>kodaeyeon</h1>
-      <h1>고대연</h1>
     </div>
   );
 }
--- client/src/shared/styled/GlobalStyle.tsx
+++ client/src/shared/styled/GlobalStyle.tsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-*{
+* {
     margin: 0px;
     padding: 0px;
     box-sizing: border-box;
