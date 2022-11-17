6ef3606febd45259160243477e340c886588e24c
philE22 <moonwhlking@gmail.com>
2022-11-17 16:49:22 +0900

    [Feat] postGallery 구현

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/gallery/entity/Gallery.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/gallery/entity/Gallery.java
@@ -4,20 +4,16 @@ import com.codestates.mainproject.oneyearfourcut.domain.artwork.entity.Artwork;
 import com.codestates.mainproject.oneyearfourcut.domain.comment.entity.Comment;
 import com.codestates.mainproject.oneyearfourcut.domain.member.entity.Member;
 import com.codestates.mainproject.oneyearfourcut.global.auditable.Auditable;
-import lombok.Getter;
-import lombok.Setter;
-
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
 @Entity
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Gallery extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,13 +21,22 @@ public class Gallery extends Auditable {
     private String title;
     private String content;
 
-    //enum의 이름을 컬럼에 저장
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) //enum의 이름을 컬럼에 저장
     private GalleryStatus status = GalleryStatus.OPEN;
+
+    @Builder
+    public Gallery(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 
     @OneToMany(mappedBy = "gallery")
     private List<Artwork> artworkList = new ArrayList<>();
--- server/oneyearfourcut/src/test/java/com/codestates/mainproject/oneyearfourcut/artwork/repository/ArtworkRepositoryTest.java
+++ server/oneyearfourcut/src/test/java/com/codestates/mainproject/oneyearfourcut/artwork/repository/ArtworkRepositoryTest.java
@@ -38,10 +38,11 @@ public class ArtworkRepositoryTest {
         member2.setNickname("작성자");
         Member writer = memberRepository.save(member2);
 
-        Gallery gallery = new Gallery();
-        gallery.setMember(admin);
-        gallery.setTitle("길동의 전시관");
-        gallery.setContent("환영합니다.");
+        Gallery gallery = Gallery.builder()
+                .title("길동의 전시관")
+                .content("환영합니다")
+                .build();
+        gallery.setMember(admin);;
         Gallery savedGallery = galleryRepository.save(gallery);
 
         Artwork artwork = new Artwork();
