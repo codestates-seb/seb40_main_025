fcdf8b0e3dfb54ae39f1fc8b0c50c585872b88dd
philE22 <moonwhlking@gmail.com>
2022-11-17 14:45:08 +0900

    [Feat] Member 서비스 로직 구현

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/member/entity/Member.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/member/entity/Member.java
@@ -7,10 +7,7 @@ import com.codestates.mainproject.oneyearfourcut.domain.comment.entity.Comment;
 import com.codestates.mainproject.oneyearfourcut.domain.gallery.entity.Gallery;
 import com.codestates.mainproject.oneyearfourcut.domain.vote.entity.Vote;
 import com.codestates.mainproject.oneyearfourcut.global.auditable.Auditable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -18,9 +15,8 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@Setter   //setter 대신 의미있는 메서드를 사용하는 것이 좋다
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // entity 생성하려면 필요한데 사용할 수 없도록 설정
 public class Member extends Auditable {
 
     @Id
@@ -30,6 +26,12 @@ public class Member extends Auditable {
     private String nickname;
 
     private String email;
+
+    @Builder    //@AllArgsConstructor를 대신
+    public Member(String nickname, String email) {
+        this.nickname = nickname;
+        this.email = email;
+    }
 
     @OneToMany(mappedBy = "member")
     private List<Gallery> galleryList = new ArrayList<>();
