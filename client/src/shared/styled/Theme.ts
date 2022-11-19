4627fa1b2ae80cee9c2ce9be3856743b031ee06e
KDY <kkdy21@naver.com>
2022-11-19 10:53:49 +0900

    🔧REFACTOR: Theme.js 수정

--- client/src/shared/styled/Theme.ts
+++ client/src/shared/styled/Theme.ts
@@ -40,8 +40,6 @@ const colors = {
   green_005: '#B1EEB2',
 };
 
-
-
 const mixins = {
   // flex
   flexBox: (
@@ -58,13 +56,13 @@ const mixins = {
       `,
 };
 
-const flex ={
-    center: "display: flex; align-items: center; justify-content: center;"
-}
+const flex = {
+  center: 'display: flex; align-items: center; justify-content: center;',
+};
 export type ColorsTypes = typeof colors;
 
 export const theme: DefaultTheme = {
-    colors,
-    mixins,
-    flex,
+  colors,
+  mixins,
+  flex,
 };
