76cd755541af7135737f2adbe5105ba8d5c4b23e
dorrion <dhdms8696@naver.com>
2022-11-29 02:35:44 +0900

    ✨FEAT :리다이렉트 URL 도착 시 토큰 저장

--- client/src/Intro/RedirectPage.tsx
+++ client/src/Intro/RedirectPage.tsx
@@ -1,9 +1,10 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { StyledLink } from 'shared/components/LinkButton/style';
-import { setStoredToken } from './hooks/tokenStorage';
+import { getStoredToken, setStoredToken } from './hooks/tokenStorage';
+import useUser from './hooks/useUser';
 
 const RedirectPage = (): ReactElement => {
-  const KakaoRedirectHandler = (): void => {
+  useEffect(() => {
     let params = new URL(document.location.toString()).searchParams;
     let access_token = params.get('access_token'); // access_token
     let refresh_token = params.get('refresh_token'); // refresh_token
@@ -14,15 +15,14 @@ const RedirectPage = (): ReactElement => {
         refresh_token: refresh_token,
       }),
     );
-  };
+    console.log(getStoredToken());
+  }, []);
+
+  // useUser.GetUser();
 
   return (
     <>
       로그인이 성공적으로 되었다는 소리다
-      <button onClick={KakaoRedirectHandler}>
-        <div>이거 누르면 토큰 저장됨.</div>
-        <div>그러면 알아서 access_token 꺼내서 헤더에 들어가용</div>
-      </button>
       <button>
         <StyledLink to={'/gallerySetting'}>전시관 구경 가기</StyledLink>
       </button>
