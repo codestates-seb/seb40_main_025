3744f338dc2107d074ec38bcc92e58f3aca8a45c
inside-eun <dameun0808@gmail.com>
2022-11-29 01:05:51 +0900

    ✨FEAT : 전시회 전체 작품 조회 구현

--- client/src/shared/hooks/useGetAllPost.tsx
+++ client/src/shared/hooks/useGetAllPost.tsx
@@ -2,22 +2,18 @@ import { useQuery } from '@tanstack/react-query';
 import { jsonInstance } from 'shared/utils/axios';
 
 const useGetAllPost = (galleryId: number) => {
-  const apis = {
-    getAllPictures: async () => {
-      return await jsonInstance.get(`galleries/${galleryId}/artworks`);
-    },
-  };
-
   const { data, status, isLoading, isSuccess } = useQuery(
     ['pictures'],
-    () => apis.getAllPictures(),
+    () => {
+      return jsonInstance.get(`galleries/${galleryId}/artworks`);
+    },
     {
-      enabled: false,
       onError(err) {
         console.log(err);
       },
     },
   );
+
   return { data, status, isLoading, isSuccess };
 };
 
