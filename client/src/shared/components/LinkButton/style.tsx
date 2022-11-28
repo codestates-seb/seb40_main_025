bdb366221acf9d2373274a7b353b30b4e3eee5f8
dorrion <dhdms8696@naver.com>
2022-11-28 13:05:44 +0900

    💄DESIGN: Link 초기화

---
+++ client/src/shared/components/LinkButton/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const StyledLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.green_001};
+  text-decoration: none;
+  outline: none;
+
+  a:hover,
+  a:active {
+    text-decoration: none;
+    color: #fff;
+  }
+`;
