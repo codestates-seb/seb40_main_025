58887e0747f43b4cb043d15b332ad28942c8e7a8
philE22 <moonwhlking@gmail.com>
2022-11-29 14:07:57 +0900

    [Fix] /docs/index.html 접근 권한 추가

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/global/config/auth/SecurityConfig.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/global/config/auth/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
                         .antMatchers(HttpMethod.GET, "/galleries/**").permitAll()
                         .antMatchers(HttpMethod.GET, "/").permitAll()
                         .antMatchers(HttpMethod.GET, "/receive-token").permitAll()
+                        .antMatchers(HttpMethod.GET, "/docs/index.html").permitAll()
 //                        .antMatchers("/members/**").hasRole("USER")
 //                        .antMatchers("/galleries/**").hasRole("USER")
 //                        .antMatchers(HttpMethod.DELETE, "/galleries/**").hasRole("USER")
