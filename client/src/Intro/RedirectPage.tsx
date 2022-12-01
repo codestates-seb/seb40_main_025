df7deadc871d36c1d680b7991b4b5f21822be1ef
dorrion <dhdms8696@naver.com>
2022-12-02 03:52:56 +0900

    🔧REFACTOR:리다이렉트 페이지 리팩토링

--- client/src/Intro/RedirectPage.tsx
+++ client/src/Intro/RedirectPage.tsx
@@ -1,40 +1,25 @@
-import { ReactElement, useEffect } from 'react';
-import { StyledLink } from 'shared/components/LinkButton/style';
+import { ReactElement } from 'react';
 import { setStoredToken, getStoredToken } from './hooks/tokenStorage';
-import { GetUser } from './hooks/useUserData';
 import { loginStore } from 'store/store';
-import { jsonInstance } from 'shared/utils/axios';
-import { getUser } from './api';
-import axios from 'axios';
+import { saveUser } from './api';
 import AutoRedirect from './hooks/AutoRedirect';
 
 const RedirectPage = (): ReactElement => {
-  const { isLoggedin, setIsLoggedIn, user } = loginStore();
-  const setUser = loginStore((state) => state.setUser);
-
   let params = new URL(document.location.toString()).searchParams;
   let access_token = params.get('access_token'); // access_token
   let refresh_token = params.get('refresh_token'); // refresh_token
 
-  setStoredToken(
-    JSON.stringify({
-      access_token: access_token,
-      refresh_token: refresh_token,
-    }),
-  );
-
-  axios
-    .get<any>(`${process.env.REACT_APP_SERVER_URL}/members/me`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: access_token,
-      },
-    })
-    .then((res) => {
-      setIsLoggedIn();
-      setUser(res.data);
-    });
-
+  // 아 그냥 localStorage로 왔을 때 토큰이 null이어서 그렇구나
+  access_token &&
+    setStoredToken(
+      JSON.stringify({
+        access_token: access_token,
+        refresh_token: refresh_token,
+      }),
+    );
+  console.log('리다이렉트 들렸어요');
+  saveUser();
+  const { user } = loginStore();
   AutoRedirect(user?.galleryId!);
 
   return <div></div>;
