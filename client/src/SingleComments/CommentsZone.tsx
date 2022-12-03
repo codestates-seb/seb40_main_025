35feef80bd2a2d7fd27dea3e09e0f2b3686d864c
inside-eun <dameun0808@gmail.com>
2022-12-03 15:01:58 +0900

    ✨FEAT : 마지막 페이지 추가

--- client/src/SingleComments/CommentsZone.tsx
+++ client/src/SingleComments/CommentsZone.tsx
@@ -3,24 +3,22 @@ import SingleComment from './SingleComment/SingleComment';
 import XIcon from 'shared/components/Icons/XIcon';
 import CommentStore from 'shared/components/PicFooter/OpenComment';
 import useGetSingleComments from './hooks/useGetSingleComments';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
 
 const CommentsList = () => {
-  const { setCloseModal, commentCount } = CommentStore();
+  const { commentCount } = CommentStore();
   const params = useParams();
   const galleryId = parseInt(params.galleryId!);
-  // const artworkId = parseInt(params.artworkId!);
+  const { state } = useLocation();
   let Page = 1;
-
-  const { data } = useGetSingleComments(galleryId, 33, Page);
-
   const navigate = useNavigate();
+  const { data } = useGetSingleComments(galleryId, state, Page);
 
   return (
     <S.CommentBody>
       <S.PicTitle>
         <S.CommentCount>댓글 {commentCount}</S.CommentCount>
-        <div onClick={() => setCloseModal()}>
+        <div onClick={() => navigate(-1)}>
           <XIcon />
         </div>
       </S.PicTitle>
