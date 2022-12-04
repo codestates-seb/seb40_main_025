9b5ef52431d1d551624838d9e9e0e732aee13f00
dorrion <dhdms8696@naver.com>
2022-12-04 12:54:45 +0900

    🔧REFACTOR: 문구도 변경

--- client/src/Gallery/components/BottonButton/index.tsx
+++ client/src/Gallery/components/BottonButton/index.tsx
@@ -20,15 +20,17 @@ const Index = ({ galleryId }: GalleryType) => {
         </Btn>
         {/* 파라미터랑 galleryId가 다를 때 */}
         {galleryId !== user?.galleryId && (
-          <Btn className='square white'>
-            <StyledLink to='/gallerySetting'>나도 전시관 만들기</StyledLink>
-          </Btn>
+          <>
+            <Btn className='square white'>
+              <StyledLink to='/gallerySetting'>나도 전시관 만들기</StyledLink>
+            </Btn>
+            <div>
+              <p>여러분만의 1년이 담긴 전시회도 만들고</p>
+              <p>친구들에게 공유해보세요!</p>
+            </div>
+          </>
         )}
       </S.BtnContainer>
-      <div>
-        <p>여러분만의 1년이 담긴 전시회도 만들고</p>
-        <p>친구들에게 공유해보세요!</p>
-      </div>
     </>
   );
 };
